68d618598174e5ffc042455590d4220196d5fad7
unknown <mnmsby4@gmail.com>
2023-10-11 17:00:07 +0530

    add js and some stiles

---
+++ script.js
@@ -0,0 +1,35 @@
+
+        document.getElementById('addToContactsButton').addEventListener('click', function() {
+            const contactData = {
+                name: 'John Doe',
+                phone: '+1234567890',
+                email: 'johndoe@example.com',
+            };
+
+            // Check if the Contacts API is available
+            if ('contacts' in navigator) {
+                navigator.contacts.select(['name', 'email', 'tel'], { multiple: true })
+                    .then(contacts => {
+                        const newContact = new Contact({
+                            name: [contactData.name],
+                            email: [contactData.email],
+                            tel: [contactData.phone],
+                        });
+
+                        const saveOptions = {
+                            contacts: [newContact],
+                        };
+
+                        return navigator.contacts.save(saveOptions);
+                    })
+                    .then(() => {
+                        alert('Contact added successfully.');
+                    })
+                    .catch(error => {
+                        console.error('Error adding contact:', error);
+                        alert('Error adding contact. Please try again later.');
+                    });
+            } else {
+                alert('Your browser does not support the Contacts API.');
+            }
+        });
