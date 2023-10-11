2295e9a69823b51ad88846cd402102a28da2e8a1
unknown <mnmsby4@gmail.com>
2023-10-11 17:19:03 +0530

    edit js

--- script.js
+++ script.js
@@ -1,19 +1,48 @@
 
-        document.getElementById('addToContactsButton').addEventListener('click', function() {
-            const contactData = {
-                name: 'John Doe',
-                phone: '+1234567890',
-                email: 'johndoe@example.com',
-            };
+        // document.getElementById('addToContactsButton').addEventListener('click', function() {
+        //     const contactData = {
+        //         name: 'John Doe',
+        //         phone: '+1234567890',
+        //         email: 'johndoe@example.com',
+        //     };
+
+        //     // Check if the Contacts API is available
+        //     if ('contacts' in navigator) {
+        //         navigator.contacts.select(['name', 'email', 'tel'], { multiple: true })
+        //             .then(contacts => {
+        //                 const newContact = new Contact({
+        //                     name: [contactData.name],
+        //                     email: [contactData.email],
+        //                     tel: [contactData.phone],
+        //                 });
+
+        //                 const saveOptions = {
+        //                     contacts: [newContact],
+        //                 };
+
+        //                 return navigator.contacts.save(saveOptions);
+        //             })
+        //             .then(() => {
+        //                 alert('Contact added successfully.');
+        //             })
+        //             .catch(error => {
+        //                 console.error('Error adding contact:', error);
+        //                 alert('Error adding contact. Please try again later.');
+        //             });
+        //     } else {
+        //         alert('Your browser does not support the Contacts API.');
+        //     }
+        // });
+
+        document.getElementById('addPhoneNumberButton').addEventListener('click', function() {
+            const phoneNumber = '+1234567890';
 
             // Check if the Contacts API is available
             if ('contacts' in navigator) {
-                navigator.contacts.select(['name', 'email', 'tel'], { multiple: true })
+                navigator.contacts.select(['tel'], { multiple: true })
                     .then(contacts => {
                         const newContact = new Contact({
-                            name: [contactData.name],
-                            email: [contactData.email],
-                            tel: [contactData.phone],
+                            tel: [phoneNumber],
                         });
 
                         const saveOptions = {
@@ -23,11 +52,11 @@
                         return navigator.contacts.save(saveOptions);
                     })
                     .then(() => {
-                        alert('Contact added successfully.');
+                        alert('Phone number added to contacts successfully.');
                     })
                     .catch(error => {
-                        console.error('Error adding contact:', error);
-                        alert('Error adding contact. Please try again later.');
+                        console.error('Error adding phone number to contacts:', error);
+                        alert('Error adding phone number to contacts. Please try again later.');
                     });
             } else {
                 alert('Your browser does not support the Contacts API.');
